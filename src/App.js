b7baae5400b1d0317762e75a09bbba386166a272
BrunnoM7 <brunno.marchetti7@gmail.com>
2021-01-09 10:18:08 -0300

    search functionality done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -10,7 +11,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -22,9 +24,15 @@ class App extends Component {
 
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} /> 
+        <SearchBox 
+          placeholder="search monster" 
+          handleChange={e => this.setState({ searchField: e.target.value}, )}
+        />
+        <CardList monsters={filteredMonsters} /> 
       </div>
     );
   }
